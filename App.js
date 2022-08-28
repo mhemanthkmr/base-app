06216cbf895b3028501b381d48caabc6f129bcaa
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-29 00:11:17 +0530

    Added a WelcomeScreen.jsx

--- App.js
+++ App.js
@@ -10,43 +10,28 @@ import {
   View,
 } from "react-native";
 import { NativeBaseProvider } from "native-base";
+import WelcomeScreen from "./components/WelcomeScreen";
 
-const handlePress = () => {
-  console.log("Pressed");
-};
+// const handlePress = () => {
+//   console.log("Pressed");
+// };
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <SafeAreaView style={styles.container}>
-        <Text onPress={handlePress}>Hello React</Text>
-        <TouchableOpacity onPress={handlePress}>
-          <Image
-            fadeDuration={300}
-            blurRadius={0}
-            borderRadius={100}
-            source={{
-              width: 200,
-              height: 200,
-              uri: "https://picsum.photos/200/500",
-            }}
-          />
-        </TouchableOpacity>
-        {/* <TouchableNativeFeedback>
-          <View
-            style={{ width: "200", backgroundColor: "blue", height: "70" }}
-          ></View>
-        </TouchableNativeFeedback> */}
-      </SafeAreaView>
+      <WelcomeScreen />
     </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     marginTop: Platform.OS == "android" ? 30 : 0,
+//     backgroundColor: "#fff",
+//     flex: 1,
+//     flexDirection: "row",
+//     justifyContent: "flex-start",
+//     alignItems: "center",
+//     flexWrap: "wrap",
+//   },
+// });
